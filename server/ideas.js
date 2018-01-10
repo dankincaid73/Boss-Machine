44ec2a7a0fc5ab0113135a8016c38decfb0a2623
Dan Kincaid <dankincaid@Dans-MacBook-Pro.local>
2018-01-10 10:47:42 -0800

    Added Logic To All Routes And Added Bonus Routes

--- server/ideas.js
+++ server/ideas.js
@@ -1,28 +1,51 @@
-const express = require('express');
-const ideasRouter = express.Router();
-const db = require("./db");
-
-// Ideas Routes
-ideasRouter.get('/api/ideas', function(req, res) {
-    const ideas = db.getAllFromDatabase('ideas');
-    res.send(ideas);
-});
-
-ideasRouter.get('/api/ideas/:id', function(req, res) {
-    // const idea = db.getFromDatabaseById('ideas');
-    // res.send(ideas);
-});
-
-ideasRouter.post('/api/ideas', function(req, res) {
-  // add ideas to db db.addToDatabase('ideas');
-});
-
-ideasRouter.put('/api/ideas', function(req, res) {
-  // update idea in db db.updateInstanceInDatabase('ideas');
-});
-
-ideasRouter.delete('/api/ideas/:id', function(req, res) {
-  // delete idea in db db.deleteFromDatabasebyId('ideas');
-});
+const ideasRouter = require('express').Router();
 
 module.exports = ideasRouter;
+
+const { 
+  addToDatabase,
+  getAllFromDatabase,
+  getFromDatabaseById,
+  updateInstanceInDatabase,
+  deleteFromDatabasebyId,
+} = require('./db');
+
+const checkMillionDollarIdea = require('./checkMillionDollarIdea');
+
+ideasRouter.param('id', (req, res, next, id) => {
+  const idea = getFromDatabaseById('ideas', id);
+  if (idea) {
+    req.idea = idea;
+    next();
+  } else {
+    res.status(404).send();
+  }
+});
+
+ideasRouter.get('/', (req, res, next) => {
+  res.send(getAllFromDatabase('ideas'));
+});
+
+ideasRouter.post('/', checkMillionDollarIdea, (req, res, next) => {
+  const newIdea = addToDatabase('ideas', req.body);
+  res.status(201).send(newIdea);
+});
+
+ideasRouter.get('/:id', (req, res, next) => {
+  res.send(req.idea);
+});
+
+ideasRouter.put('/:id', checkMillionDollarIdea, (req, res, next) => {
+  let updatedInstance = updateInstanceInDatabase('ideas', req.body);
+  res.send(updatedInstance);
+});
+
+ideasRouter.delete('/:id', (req, res, next) => {
+  const deleted = deleteFromDatabasebyId('ideas', req.params.id);
+  if (deleted) {
+    res.status(204);
+  } else {
+    res.status(500);
+  }
+  res.send();
+});
